66a10081eed78b6c69872141fb7d8ce6e7163fd3
André Oliveira <andrelitoscria@gmail.com>
2024-03-06 18:39:28 -0300

    .
    .

--- exc1.js
+++ exc1.js
@@ -27,7 +27,36 @@ function adicionar(){
         let item = document.createElement('option')
         item.text = `Valor ${num.value} foi adicionado`
         lista.appendChild(item)
+        res.innerHTML = ''
    } else{
     alert('numero invalido ou já está na lista')
-   } 
+   }
+   num.value = ''
+   num.focus()
+}
+
+function verificar(){
+    if(valores.length == 0){
+        alert("adicione alguma coisa para finalizar")
+    }else{
+        let tdn = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        for(let pos in valores){
+            soma = soma + valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+             if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+        let media = soma/tdn
+
+        res.innerHTML = ''
+        res.innerHTML += `Ao todo temos ${tdn} numeros cadastrados.`
+        res.innerHTML += `<p>o maior numero foi ${maior}.</p>`
+        res.innerHTML += `<p>o menor numero foi ${menor}.</p>`
+        res.innerHTML += `<p>a soma deles são ${soma}.</p>`
+        res.innerHTML += `<p>a media dos numeros são ${media}</p>`
+    }
 }
